a38aa714424a524e45ac1d1817ef87985300106c
SSAKIS47 <skiadaskonstantinos8@gmail.com>
2024-06-10 17:16:30 +0300

    Updates
    2nd page change

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var p1_re = document.getElementById('p1_re');
 var p2_re = document.getElementById('p2_re');
 var mou_re = document.getElementById('mou_re');
 
+
+
 var counter = 1;
 document.getElementById('mouse').addEventListener('click', function() {
     if (counter == 1) {
@@ -20,3 +22,29 @@ document.getElementById('mouse').addEventListener('click', function() {
 });
 
 
+function changeContent(counter) {
+
+    var text = document.getElementById('text');
+    var re = document.getElementById('Replace');
+
+    if (counter == 2) {
+        var temp = text.innerHTML;
+
+        text.innerHTML = re.innerHTML;
+
+        re.innerHTML = temp;
+    } else if (counter == 1) {
+        var temp1 = re.innerHTML;
+
+        re.innerHTML = div1.innerHTML;
+
+        text.innerHTML = temp1;
+    }
+}
+
+
+function changeId() {
+    const element = document.getElementById("text");  // Get the DIV element
+    element.classList.remove("text"); // Remove mystyle class from DIV
+    element.classList.add("Replace"); // Add newone class to DIV
+}
